d6b8445f8f942addf92807906e892a4c991e8a1c
s-shchekoldin <shchekoldin@gmail.com>
2024-08-06 21:12:19 +0100

    improve perfomance, less branches

--- httpResp.cc
+++ httpResp.cc
@@ -1,6 +1,6 @@
 // ==============================================================
-// Date: 2024-07-24 19:59:27 GMT
-// Generated using vProto(2024.07.24)         https://www.vsyn.ru
+// Date: 2024-08-06 19:58:59 GMT
+// Generated using vProto(2024.08.06)         https://www.vsyn.ru
 // Author: Sergey V. Shchekoldin     Email: shchekoldin@gmail.com
 // ==============================================================
 
@@ -113,7 +113,7 @@ inline void httpResp::parse(state_t & state)
             case node_t::NO_STATE:
             default: return;
         }; // switch
-        if (n == state.node && d == state.data)
+        if (d == state.data && n == state.node) [[unlikely]]
             break;
     }
 }
